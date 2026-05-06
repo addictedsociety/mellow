9a00468e1db4476557d6724125a4bd00e5c1955a
addictedsociety <addictedsociety666@gmail.com>
2026-05-06 19:59:15 +0200

    chore: add formatting setup and update app polish

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+import js from '@eslint/js'
+import prettier from 'eslint-config-prettier'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+
+export default tseslint.config(
+  {
+    ignores: [
+      'dist',
+      'node_modules',
+      'src-tauri/target',
+      'src-tauri/gen',
+      'src/components/ui',
+      'coverage',
+      '*.config.*.timestamp-*'
+    ]
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.{ts,vue}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.es2024
+      },
+      parserOptions: {
+        parser: tseslint.parser
+      }
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/no-v-html': 'off',
+      'vue/max-attributes-per-line': 'off',
+      'vue/html-self-closing': [
+        'error',
+        {
+          html: {
+            void: 'always',
+            normal: 'always',
+            component: 'always'
+          },
+          svg: 'always',
+          math: 'always'
+        }
+      ]
+    }
+  },
+  prettier
+)
